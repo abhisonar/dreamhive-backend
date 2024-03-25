8d84993035e5a5f12757c0f96128511713c7b657
Abhishek Sonar <abhisonar4121@gmail.com>
2024-03-25 21:52:25 +0530

    chore(package): added jsonwebtoken and bcrypt package

--- apps/dreamer/src/main.ts
+++ apps/dreamer/src/main.ts
@@ -5,8 +5,15 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+//json
+app.use(express.json());
+
+// cors
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 
 app.listen(port, host, () => {
